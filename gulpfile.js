937d4ef85a383c0fe7d8347770f516c83bbe66c8
agapovk <agapovk@gmail.com>
2021-12-06 17:34:14 +0500

    chapter 3

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,37 @@
-function defaultTask(cb) {
-    // place code for your default task here
-    cb();
-  }
-  
-  exports.default = defaultTask
+const { src, dest, parallel, series } = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
+const pug = require('gulp-pug');
+const browserSync = require('browser-sync').create();
 
-  var gulp = require('gulp'),
-  svgSprite = require('gulp-svg-sprite');
- 
-gulp.src('path/to/assets/*.svg')
-  .pipe(svgSprite(/* ... Insert your configuration here ... */))
-  .pipe(gulp.dest('out'));
+const browserSyncJob = () => {
+  browserSync.init({
+    server: "build/"
+  });
+
+  watch('app/scss/*.scss', buildSass);
+  watch('app/*.pug', buildPug);
+};
+
+const buildSass = () => {
+  console.log('Компиляция SASS');
+
+  return src('app/scss/*.scss')
+    .pipe(sass())
+    .pipe(dest('build/styles/'))
+    .pipe(browserSync.stream());
+}
+
+const buildPug = () => {
+  console.log('Компиляция Pug');
+
+  return src('app/*.pug')
+    .pipe(pug(
+      {pretty: true}
+    ))
+    .pipe(dest('build/'))
+    .pipe(browserSync.stream());
+}
+
+exports.server = browserSyncJob;
+exports.build = parallel(buildSass, buildPug);
+exports.go = series(build, server);
